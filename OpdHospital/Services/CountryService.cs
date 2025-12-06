cef1a277ff8cbf7093e7ed262a73dd025dc0986a
Lakhan Verma <lakhan@example.com>
2025-12-06 14:08:58 +0530

    git changes for services

---
+++ OpdHospital/Services/CountryService.cs
@@ -0,0 +1,14 @@
+﻿using OpdHospital.Interfaces.IGenericRepositories;
+using OpdHospital.Models.Countries;
+
+namespace OpdHospital.Services
+{
+    public class CountryService
+    {
+        private IGenericRepository<Country> _genericRepository;
+        public CountryService(IGenericRepository<Country> genericRepository)
+        {
+            _genericRepository = genericRepository;
+        }
+    }
+}
---
+++ OpdHospital/Services/GenericService.cs
@@ -0,0 +1,53 @@
+﻿using OpdHospital.Interfaces;
+using OpdHospital.Interfaces.IGenericRepositories;
+
+namespace OpdHospital.Services
+{
+    public class GenericService<T> : IGenericService<T> where T : class
+    {
+        private readonly IGenericRepository<T> _genericRepository;
+
+        public GenericService(IGenericRepository<T> genericRepository)
+        {
+            _genericRepository = genericRepository;
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            await _genericRepository.AddAsync(entity);
+            return entity;
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var entity = await _genericRepository.GetByIdAsync(id);
+            if (entity == null)
+                return false;
+
+            await _genericRepository.DeleteAsync(entity);
+
+            return true;
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _genericRepository.GetAllAsync();
+        }
+
+        public async Task<T?> GetByIdAsync(int id)
+        {
+            return await _genericRepository.GetByIdAsync(id);
+        }
+
+        public async Task<T?> UpdateAsync(int id, T entity)
+        {
+            var existing = await _genericRepository.GetByIdAsync(id);
+            if (existing == null)
+                return null;
+
+            await _genericRepository.UpdateAsync(entity);
+
+            return entity;
+        }
+    }
+}
