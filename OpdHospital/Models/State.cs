ef036ee2e5010b5132b1f7185a82fa1baa8a9198
Balwant Singh <balwant@Balwants-MacBook-Air.local>
2025-12-14 18:40:40 +0530

    state model updated

--- OpdHospital/Models/State.cs
+++ OpdHospital/Models/State.cs
@@ -6,11 +6,9 @@ namespace OpdHospital.Models
     public class State : BaseEntity
     {
         [Key]
-        public int Id { get; set; }
-        [ForeignKey("Country")]
+        public int StateId { get; set; }
         public int CountryId { get; set; }
-        public Country Country { get; set; }
-        public string Name { get; set; }
+        public string StateName { get; set; }
         public string StateCode { get; set; }
     }
 }
